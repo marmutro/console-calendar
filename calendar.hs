96be1d1010342612b40a260b8450e377b959a3c0
Martin Troxler <marmutro@gmail.com>
2015-10-06 20:46:52 +0200

    Fixed week formatting

--- calendar.hs
+++ calendar.hs
@@ -5,33 +5,50 @@ import Data.Time.Format
 import System.Locale
 import Text.Printf
 
+listToString :: [a] -> (a -> String) -> String
+listToString ls formatter = foldr (\v acc -> formatter v ++ acc) "" ls
+
+
 datesInYear year = [beginOfYear year..endOfYear year] where
     beginOfYear year = fromGregorian year 1 1
     endOfYear year = addDays (-1) $ beginOfYear (year+1)
     
-monthOfDate date = month
-    where (_, month, _) = toGregorian date
+monthOfDate date = month where
+    (_, month, _) = toGregorian date
     
-dayOfDate date = day
-    where (_, _, day) = toGregorian date
     
-weekNumber date = week
-    where (week, _) = mondayStartWeek date
+weekNumber date = week where
+    (week, _) = mondayStartWeek date
 
-monthName date = formatTime defaultTimeLocale "%B" date
+weekDay date = day where
+    (_, day) = mondayStartWeek date
+    
+dayOfDate date = day where
+    (_, _, day) = toGregorian date
 
-listToString :: [a] -> (a -> String) -> String
-listToString ls formatter = foldr (\v acc -> formatter v ++ acc) "" ls
+
+monthName date = printf "%-21s" $ formatTime defaultTimeLocale "%B" date
+
+byMonth = groupBy (\a b -> (monthOfDate a) == (monthOfDate b))
+byWeek = groupBy (\a b -> (weekNumber a) == (weekNumber b))
+monthByWeek = map byWeek
+
+padWeekFront :: [Day] -> String
+padWeekFront ls = replicate (3 * ((weekDay $ head ls) - 1)) ' '
+padWeekBack :: [Day] -> String
+padWeekBack ls = replicate (3 * (7 - (weekDay $ last ls))) ' '
+
+showWeek :: [Day] -> String
+showWeek ls = frontPadding ++ days ++ backPadding where
+    frontPadding = padWeekFront ls
+    days = listToString (map dayOfDate ls) (\d -> printf "%3d" d)
+    backPadding = padWeekBack ls
+    
+showMonth :: [[Day]] -> [String]
+showMonth ls = [(monthName $ head $ head ls)] ++ (map showWeek ls)
+monthToLines :: [[[Day]]] -> [String]
+monthToLines ls = concat $ map showMonth ls
 
 main = do 
-    let byMonth = groupBy (\a b -> (monthOfDate a) == (monthOfDate b))
-    let byWeek = groupBy (\a b -> (weekNumber a) == (weekNumber b))
-    let monthByWeek = map byWeek
-    let printWeek ls = do
-        -- putStr $ show (weekNumber $ head ls) ++ ":"
-        putStr $ replicate (3*(7 - length ls)) ' ' 
-        putStrLn $ listToString (map dayOfDate ls) (\v -> printf "%3d" v)
-    let printMonth ls = do
-        putStrLn $ monthName $ head $ head ls
-        mapM_ printWeek ls
-    mapM_ printMonth (monthByWeek . byMonth $ datesInYear 2015)
+    let lines = monthToLines (monthByWeek . byMonth $ datesInYear 2015)
+    mapM_ print lines
